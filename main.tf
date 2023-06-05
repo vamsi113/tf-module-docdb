ceef1a2adb84df18b66cc6aa55c7216f821cb836
vamsi113 <krishultimate5@gmail.com>
2023-06-05 13:31:00 -0400

    change

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
+resource "aws_docdb_subnet_group" "default" {
+  for_each   = var.docdb
+  name       = "${var.env}-${each.key}-roboshop-docdb"
+  subnet_ids = var.subnets
+
+  tags = {
+    Name = "${var.env}-${each.key}-roboshop-docdb"
+  }
+}
+
 resource "aws_docdb_cluster" "docdb" {
   for_each                = var.docdb
-  cluster_identifier      = "${var.env}-roboshop-docdb"
+  cluster_identifier      = "${var.env}-${each.key}-roboshop-docdb"
   engine                  = each.value.engine
   master_username         = "foo"
   master_password         = "mustbeeightchars"
   skip_final_snapshot     = true
+  db_subnet_group_name    = "${var.env}-${each.key}-roboshop-docdb"
 }
--- vars.tf
+++ vars.tf
@@ -1,2 +1,3 @@
 variable "docdb" {}
 variable "env" {}
+variable "subnets" {}
